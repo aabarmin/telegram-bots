8273f3c9bd6cc341197b608c6fca5696ec275e40
Aleksandr Barmin <abarmin@outlook.com>
2023-12-25 16:48:42 +0000

    feat: create a new Spring Boot project

---
+++ src/main/java/dev/abarmin/bots/BotsApplication.java
@@ -0,0 +1,13 @@
+package dev.abarmin.bots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/abarmin/bots/BotsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abarmin.bots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
