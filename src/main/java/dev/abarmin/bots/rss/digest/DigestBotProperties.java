918e6edf7b2b22cc101f01d7a29862d4f05e4d51
Aleksandr Barmin <abarmin@outlook.com>
2023-12-27 12:17:12 +0000

    feat: Remove SQS and get back to polling

--- src/main/java/dev/abarmin/bots/rss/digest/DigestBotProperties.java
+++ src/main/java/dev/abarmin/bots/rss/digest/DigestBotProperties.java
@@ -6,8 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("telegram.bot.rss.digest")
 public record DigestBotProperties(
         @NotEmpty String botName,
-        @NotEmpty String botToken,
-        @NotEmpty String webHookUrl,
-        @NotEmpty String sqsQueue
+        @NotEmpty String botToken
 ) {
 }
