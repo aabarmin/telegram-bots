91dcaf8007e31b62345f66885feba0d27aff49f6
Aleksandr Barmin <abarmin@outlook.com>
2024-01-05 16:56:20 +0000

    feat: Admin panel

--- src/main/java/dev/abarmin/bots/listener/persistence/TelegramBotChatRepository.java
+++ src/main/java/dev/abarmin/bots/listener/persistence/TelegramBotChatRepository.java
@@ -1,9 +1,18 @@
 package dev.abarmin.bots.listener.persistence;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 import java.util.Optional;
 
-public interface TelegramBotChatRepository extends ListCrudRepository<TelegramBotChat, Integer> {
+public interface TelegramBotChatRepository extends
+        ListCrudRepository<TelegramBotChat, Integer>,
+        ListPagingAndSortingRepository<TelegramBotChat, Integer> {
+
+    /**
+     * Find chat by chat id.
+     * @param chatId
+     * @return
+     */
     Optional<TelegramBotChat> findByChatId(long chatId);
 }
