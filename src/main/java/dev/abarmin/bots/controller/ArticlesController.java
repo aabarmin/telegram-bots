e6d413c71f50582b78bbff870823d4ad61825587
Aleksandr Barmin <abarmin@outlook.com>
2024-04-11 16:45:18 +0100

    Add filter by sources

--- src/main/java/dev/abarmin/bots/controller/ArticlesController.java
+++ src/main/java/dev/abarmin/bots/controller/ArticlesController.java
@@ -3,11 +3,13 @@ package dev.abarmin.bots.controller;
 import dev.abarmin.bots.controller.model.ArticleEpisode;
 import dev.abarmin.bots.controller.model.ArticleRow;
 import dev.abarmin.bots.entity.episodes.EpisodeArticle;
+import dev.abarmin.bots.repository.ArticleSourceRepository;
 import dev.abarmin.bots.repository.EpisodeArticlesRepository;
 import dev.abarmin.bots.repository.EpisodesRepository;
 import dev.abarmin.bots.scheduler.JdbcHelper;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Controller;
@@ -17,7 +19,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,13 +30,17 @@ public class ArticlesController {
     private final JdbcClient jdbcClient;
     private final EpisodesRepository episodesRepository;
     private final EpisodeArticlesRepository episodeArticlesRepository;
+    private final ArticleSourceRepository articleSourceRepository;
 
     @GetMapping("/articles")
     public ModelAndView index(ModelAndView modelAndView,
-                              @RequestParam(value = "show_all", defaultValue = "false") boolean showAll) {
+                              @RequestParam(value = "show_all", defaultValue = "false") boolean showAll,
+                              @RequestParam(value = "sources", defaultValue = "") String sourcesString) {
         modelAndView.addObject("episodes", episodesRepository.findAll());
         modelAndView.addObject("selected", new ArticlesList());
-        modelAndView.addObject("articles", getArticles(showAll));
+        modelAndView.addObject("articles", getArticles(showAll, getSources(sourcesString)));
+        modelAndView.addObject("sources", articleSourceRepository.findAll());
+        modelAndView.addObject("selectedSources", getSources(sourcesString));
         modelAndView.addObject("showAll", showAll);
         modelAndView.setViewName("articles/index");
         return modelAndView;
@@ -57,7 +66,16 @@ public class ArticlesController {
                 ea.getArticleId().getId()).isEmpty();
     }
 
-    private Collection<ArticleRow> getArticles(boolean showAll) {
+    private Collection<Integer> getSources(String sourcesString) {
+        if (StringUtils.isEmpty(sourcesString)) {
+            return List.of();
+        }
+        return Arrays.stream(sourcesString.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+    }
+
+    private Collection<ArticleRow> getArticles(boolean showAll, Collection<Integer> sources) {
         String query = """
                 select
                     article.article_id,
@@ -72,15 +90,28 @@ public class ArticlesController {
 
         if (!showAll) {
             query += """
-                left join episodes_articles ea on ea.article_id = article.article_id
-                where ea.episode_id is null
-            """;
+                        left join episodes_articles ea on ea.article_id = article.article_id
+                        where ea.episode_id is null
+                    """;
+
+        }
+        if (!sources.isEmpty()) {
+            if (showAll) {
+                query += "where ";
+            } else {
+                query += "and ";
+            }
+            query += " source.source_id in (:sources) ";
         }
         query += """
-        order by article.article_added desc
-        """;
+                order by article.article_added desc
+                """;
 
-        return jdbcClient.sql(query)
+        JdbcClient.StatementSpec sqlSpec = jdbcClient.sql(query);
+        if (!sources.isEmpty()) {
+            sqlSpec.param("sources", sources);
+        }
+        return sqlSpec
                 .query((rs, rowNum) -> ArticleRow.builder()
                         .articleId(rs.getInt("article_id"))
                         .articleTitle(rs.getString("article_title"))
